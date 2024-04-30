c80ac49c5c1cd04ed4d10e295e4e3a4aa7e22616
teguhbayupratama <teguhbayupratama@gmail.com>
2024-04-30 10:23:48 +0700

    modified:   src/app/_components/Data.tsx 	modified:   src/app/page.tsx

--- src/app/_components/Data.tsx
+++ src/app/_components/Data.tsx
@@ -1,32 +1,7 @@
 import { getData } from "../action";
 import { title } from "@/types/data.types";
 
-const seeder = [
-  {
-    time: "76767867867",
-    judul: "tod",
-  },
-  {
-    time: "32832982",
-    judul: "makan",
-  },
-  {
-    time: "87328392372",
-    judul: "siah suka blud",
-  },
-  {
-    time: "809832980984",
-    judul: "kjalkjlkass",
-  },
-  {
-    time: "2098809280982",
-    judul: "aosjkasjjas",
-  },
-  {
-    time: "298882098928932",
-    judul: "aksjkajskjsa",
-  },
-];
+export const revalidate = 0
 
 export default async function Data() {
   const datas = (await getData()) as title[];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Form from "./_components/Form";
 import Data from "./_components/Data";
 
+export const revalidate = 0
+
 export default function Home() {
   return (
     <main className="flex max-h-screen flex-col items-center gap-8 p-28">
